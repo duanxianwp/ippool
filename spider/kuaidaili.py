fc6950b898924f98c287b39f7eb30dba1d4bafde
wp <18888106880@163.com>
2018-08-22 13:18:17 +0800

    添加普通代理抓取 多进程加锁 copy防止出现同objectid

--- spider/kuaidaili.py
+++ spider/kuaidaili.py
@@ -3,26 +3,51 @@ import requests
 from db import ip_validate
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
+from multiprocessing import Lock
 from db.mongo_driver import MongoDB
 
 mongo = MongoDB()
+lock = Lock()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 }
 
 
-def get_ip(page):
+def query_and_store_ip(page):
+    items = []
+    items.extend(get_inha_ip(page))
+    items.extend(get_intr_ip(page))
+    for item in items:
+        lock.acquire()
+        store_ip(item)
+        lock.release()
+
+
+def get_inha_ip(page):
     url = 'https://www.kuaidaili.com/free/inha/{}/'.format(page)
+    return get_ip(url, page)
+
+
+def get_intr_ip(page):
+    url = 'https://www.kuaidaili.com/free/intr/{}/'.format(page)
+    return get_ip(url, page)
+
+
+def get_ip(url, page):
     source = requests.get(url, headers=headers).text
+
+    if source == '-10':
+        return get_ip(url, page)
     soup = BeautifulSoup(source, 'lxml')
     ips = soup.select('.table > tbody > tr > td:nth-of-type(1)')
     ports = soup.select('.table > tbody > tr > td:nth-of-type(2)')
+    items = []
     item = {}
     for ip, port in zip(ips, ports):
         item['ip'] = ip.text
         item['port'] = port.text
-        '''使用copy'''
-        store_ip(copy.copy(item))
+        items.append(copy.copy(item))
+    return items
 
 
 def store_ip(item):
@@ -47,5 +72,5 @@ def run():
     begin = 1
     end = 100
     pool = Pool()
-    pool.map(get_ip, [page for page in range(begin, end + 1)])
+    pool.map(query_and_store_ip, [page for page in range(begin, end + 1)])
     pool.close()
